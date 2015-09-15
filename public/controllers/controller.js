25ed2081119ead2a6192d81eb898910ad516b691
Michael Chun <chunm404@gmail.com>
2015-09-14 18:22:06 -0700

    Many Changes, still needs work though
    Might want to go back and redo includes.

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,106 +1,108 @@
-var myApp = angular.module('myApp', []);
-myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    //console.log("Reached controller");
+var myApp = angular.module('myApp', ['angularUtils.directives.dirPagination']);
+(function () {
+    myApp.controller('AppCtrl', ['$http', function ($http) {
+        "use strict";
+        var jspListCtrl = this;
 
-    /* Array of JSPs */
-    $scope.jsps = [{id: 'jsp1'}];
+        var refresh = function () {
+            $http.get('/urlList').success(function (response) {
+                console.log("GET Successful");
+                jspListCtrl.urlList = response;
+                jspListCtrl.entry = "";
+            });
+        };
 
-    var refresh = function() {
-        $http.get('/urlList').success(function(response) {
-            console.log("GET Successful");
-            $scope.urlList = response;
-            $scope.entry = "";
-			//console.log(response);
-        });
-    };
+        jspListCtrl.sort = "";
+        jspListCtrl.setSort = function(option) {
+            if (option === jspListCtrl.sort) {
+                jspListCtrl.sort = "-"+option;
+            } else {
+                jspListCtrl.sort = option;
+            }
+        };
 
-    var getEntryJSP = function() {
-        var jspString = "";
-        for(var jsp in $scope.jsps){
-            jspString += $scope.jsps[jsp].value + ",";
-        }
-        jspString = jspString.substring(0, jspString.length - 1);
-        //console.log('JSP STRING: ' + jspString);
-        return jspString;
-    };
+        /* Array of JSPs */
+        jspListCtrl.jsps = [{id: 'jsp1', value: ""}];
 
-    var getEditJSP = function(response) {
-        $scope.jsps = [];
-        var newJspNo = 1;
-        var res = response.jsp.split(",");
-        for(var i =0; i<res.length;i++){
-            $scope.jsps.push({'id': 'jsp'+newJspNo, 'value': res[i]});
-            newJspNo = $scope.jsps.length+1;
-        }
-    };
+        refresh();
 
-    refresh();
+        /* helper function, converts include array to string */
+        var getEntryJSP = function() {
+            var jspString = "";
+            for(var jsp in jspListCtrl.jsps){
+                if(jspListCtrl.jsps[jsp].value !== undefined){
+                    jspString += jspListCtrl.jsps[jsp].value + ",";
+                }
+            }
+            jspString = jspString.substring(0, jspString.length - 1);
+            return jspString;
+        };
 
-    $scope.addEntry = function() {
-        $scope.entry.jsp = getEntryJSP();
-        $http.post('/urlList', $scope.entry).success(function(response) {
-            refresh();
-            console.log($scope.entry + " Added");
-        });
-    };
+        /* helper function, converts include string to array */
+        var getEditJSP = function(response) {
+            jspListCtrl.jsps = [];
+            var newJspNo = 1;
+            var res = response.jsp.split(",");
+            for(var i =0; i<res.length;i++){
+                jspListCtrl.jsps.push({'id': 'jsp'+newJspNo, 'value': res[i]});
+                newJspNo = jspListCtrl.jsps.length+1;
+            }
+        };
 
-    $scope.remove = function(id) {
-        console.log(id + "removed");
-        $http.delete('/urlList/' + id).success(function(response) {
-            refresh();
-        });
-    };
+        jspListCtrl.addEntry = function () {
+            jspListCtrl.entry.jsp = getEntryJSP();
+            console.log(jspListCtrl.entry);
+            $http.post('/urlList', jspListCtrl.entry).success(function (response) {
+                refresh();
+                console.log(jspListCtrl.entry + " Added");
+            });
+        };
 
-    $scope.edit = function(id) {
-        console.log(id + " Edited");
-        $http.get('/urlList/' + id).success(function(response) {
-            $scope.entry = response;
-            getEditJSP($scope.entry);
-        });
-    };
+        jspListCtrl.remove = function (id) {
+            console.log(id + "removed");
+            $http.delete('/urlList/' + id).success(function (response) {
+                refresh();
+            });
+        };
 
-    $scope.update = function() {
-        $scope.entry.jsp = getEntryJSP();
-        console.log($scope.entry._id + " Updated");
-        $http.put('/urlList/' + $scope.entry._id, $scope.entry).success(function(response) {
-            refresh();
-        })
-    };
+        jspListCtrl.edit = function (id) {
+            console.log(id + " Edited");
+            $http.get('/urlList/' + id).success(function (response) {
+                jspListCtrl.entry = response;
+                getEditJSP(jspListCtrl.entry);
+                console.log("Entrys " + jspListCtrl.entry.jsp);
+            });
+        };
 
-    $scope.mySplit = function(string, nb) {
-        if(string !== undefined){
-            $scope.array = string.split(',');
-            return $scope.result = $scope.array[nb];
-        }
-        else{
-            return $scope.result;
-        }
-    }
+        jspListCtrl.update = function () {
+            console.log(jspListCtrl.entry);
+            jspListCtrl.entry.jsp = getEntryJSP();
+            console.log(jspListCtrl.entry._id + " Updated");
+            $http.put('/urlList/' + jspListCtrl.entry._id, jspListCtrl.entry).success(function (response) {
+                refresh();
+            });
+        };
 
-    /* Add JSP */
-    $scope.addNewJSP = function(){
-        var newJspNo = $scope.jsps.length+1;
-        $scope.jsps.push({'id': 'jsp'+newJspNo});
-    };
+        /* Adds JSP, add button in add entry */
+        jspListCtrl.addNewJSP = function () {
+            var newJspNo = jspListCtrl.jsps.length + 1;
+            jspListCtrl.jsps.push({'id': 'jsp' + newJspNo, 'value': ""});
+        };
 
-    /* Remove JSP */
-    $scope.removeJSP = function(){
-        var lastItem = $scope.jsps.length-1;
-        if(lastItem !== 0){
-            $scope.jsps.splice(lastItem);
-        }
-    };
+        /* Removes JSP, regular table action */
+        jspListCtrl.removeJSP = function () {
+            var lastItem = jspListCtrl.jsps.length - 1;
+            if (lastItem !== 0 || lastItem < 0) {
+                jspListCtrl.jsps.splice(lastItem);
+            }
+        };
 
-    /* Reset Include Field */
-    $scope.resetJSP = function(){
-        $scope.jsps = [{id: 'jsp1'}];
-        $scope.entry = "";
-    };
+        /* Resets Include Fields, clear button in add entry */
+        jspListCtrl.resetJSP = function () {
+            jspListCtrl.jsps = [{id: 'jsp1', value: ""}];
+            jspListCtrl.entry = "";
+        };
 
-}]);
+    }]);
 
-myApp.filter('trustAsHTML', ['$sce', function($sce){
-    return function(text) {
-        return $sce.trustAsHtml(text);
-    };
-}]);
+})();
